81d07f08b914c7d631d09bc46e6f61b5a436257f
wangerxiao <863579016@qq.com>
2016-04-25 09:19:08 +0800

    class to string

---
+++ class/class6_tostring.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+class Student(object):
+	def __init__(self,name):
+		self.name=name
+	def __str__(self):
+		return 'Student object (name:%s)!' % self.name
+
+print Student('Robin')
