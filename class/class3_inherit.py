a5f1b933c7c813a8f91dc2ab930b53da16e46621
wangerxiao <863579016@qq.com>
2016-04-25 08:30:52 +0800

    class inherit

---
+++ class/class3_inherit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pyhton
+
+class Animal(object):
+	def run(self):
+		print "Animal is running"
+
+class Dog(Animal):
+	pass
+
+class Cat(Animal):
+	pass
+
+
+dog=Dog()
+cat=Cat()
+
+dog.run()
+cat.run()
